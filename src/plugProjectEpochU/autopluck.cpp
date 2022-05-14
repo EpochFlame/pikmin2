dcb2f0d22b50cf2793854843c3512f3ed8c20650
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-05-14 16:36:58 -0400

    251-style treasure cutscene skip

--- src/plugProjectEpochU/autopluck.cpp
+++ src/plugProjectEpochU/autopluck.cpp
@@ -1,6 +1,12 @@
 #include "types.h"
+#include "Dolphin/os.h"
 #include "Game/GameSystem.h"
 #include "Game/TimeMgr.h"
+#include "Game/MoviePlayer.h"
+#include "JSystem/JUT/JUTGamePad.h"
+
+bool isTreasureCutscene;
+
 namespace Game {
 
 struct Navi {
@@ -12,14 +18,28 @@ struct NaviWalkState {
 };
 
 void autopluck(NaviWalkState* walkstate, Navi* captain)
+// performs Pikmin 3-style autopluck of pikmin seeds
 {
 	captain->procActionButton();
 	walkstate->execAI(captain);
 }
 
-u32 getCurrentDay(Game::GameSystem* game)
+// returns the current day, as displayed in the in-game UI
+u32 getCurrentDay(Game::GameSystem* game) { return game->m_timeMgr->m_dayCount + 1; }
+
+void allowSkipTreasureCutscene(MoviePlayer* movieplayer)
+// allows 251-style treasure cutscene skips
 {
-	return game->m_timeMgr->m_dayCount + 1;
+	if (isTreasureCutscene && movieplayer != nullptr) {
+		if (movieplayer->m_demoState == 6) {
+			isTreasureCutscene = false;
+			// OSReport("Treasure cutscene finished normally!");
+		} else if (((JUTGamePad::mPadStatus.m_stats->_00 & 0x1000) != 0) && (movieplayer->m_demoState == 5)) {
+			isTreasureCutscene = false;
+			movieplayer->skip();
+			// OSReport("Treasure cutscene skipped successfully!);
+		}
+	}
 }
 
 }; // namespace Game
