4a2bd27d4c7dc4c97cae732f78480b05e55df1bf
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-07-14 15:38:31 -0400

    autopluck
    dismiss captain to pluck or press a once

--- src/moddingU/main.cpp
+++ src/moddingU/main.cpp
@@ -1,4 +1,27 @@
 #include "types.h"
+#include "Dolphin/os.h"
+#include "Game/GameSystem.h"
+#include "Game/TimeMgr.h"
+#include "Game/MoviePlayer.h"
+#include "JSystem/JUT/JUTGamePad.h"
+
+namespace Game {
+
+struct Navi {
+	void procActionButton();
+};
+
+struct NaviWalkState {
+	void execAI(Navi*);
+};
+
+void autopluck(NaviWalkState* walkstate, Navi* captain)
+// performs Pikmin 3-style autopluck of pikmin seeds
+{
+	captain->procActionButton();
+	walkstate->execAI(captain);
+}
+}; // namespace Game
 
 namespace mod {
 
